7ad9402d0242855dfaefc8a359a87d2467faa393
eduardo.briceno <eduardo.briceno@playtika.com>
2019-01-27 21:01:50 -0300

    server edit

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
+const port = process.env.PORT || 5000;
 const mongoose = require('mongoose');
 const Forecast = require('./api/models/apiModel');
 
@@ -11,16 +12,16 @@ mongoose.connect('mongodb://heroku_24fzlm7k:s23et5m2j414a8t2pos9enu00k@ds213755.
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const routes = require('./api/routes/apiRoute');
+routes(app);
+
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
   app.use(express.static(path.join(__dirname, 'client/build')));
   // Handle React routing, return all requests to React app
   app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
 }
 
-const routes = require('./api/routes/apiRoute');
-routes(app);
-
-app.listen(process.env.PORT || 5000, () => console.log(`Listening...`));
+app.listen(port, () => console.log(`Listening on port ${port}`));
